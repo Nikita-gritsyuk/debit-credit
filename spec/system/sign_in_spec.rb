f3f996e5bf680bc19ed0e7cdd591a086101ae225
ng <nikita.gritsyuk@gmail.com>
2022-07-31 20:36:37 +0300

    Bootstrap 5 integration

--- spec/system/sign_in_spec.rb
+++ spec/system/sign_in_spec.rb
@@ -9,31 +9,31 @@ describe 'User signs in', type: :system do
   scenario 'valid with correct credentials' do
     fill_in 'user_email', with: @user.email
     fill_in 'user_password', with: @user.password
-    click_button 'Log in'
+    click_button 'Sign in'
 
     expect(page).to have_text 'Signed in successfully.'
 
-    expect(page).to have_button 'Logout'
+    expect(page).to have_link 'Sign out'
     expect(page).to have_current_path root_path
   end
 
   scenario 'invalid with unregistered account' do
     fill_in 'user_email', with: Faker::Internet.email
     fill_in 'user_password', with: 'FakePassword'
-    click_button 'Log in'
+    click_button 'Sign in'
 
     expect(page).to have_no_text 'Signed in successfully.'
     expect(page).to have_text 'Invalid Email or password.'
-    expect(page).to have_no_button 'Logout'
+    expect(page).to have_no_link 'Sign out'
   end
 
   scenario 'invalid with invalid password' do
     fill_in 'user_email', with: @user.email
     fill_in 'user_password', with: 'FakePassword123'
-    click_button 'Log in'
+    click_button 'Sign in'
 
     expect(page).to have_no_text 'Signed in successfully.'
     expect(page).to have_text 'Invalid Email or password.'
-    expect(page).to have_no_button 'Logout'
+    expect(page).to have_no_link 'Sign out'
   end
 end
