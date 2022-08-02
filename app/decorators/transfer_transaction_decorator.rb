45df2956896cc436365e8f50087dd4b54a2a4e28
ng <nikita.gritsyuk@gmail.com>
2022-08-03 01:35:37 +0300

    transfer_decorator minor methods ranaming and specs

--- app/decorators/transfer_transaction_decorator.rb
+++ app/decorators/transfer_transaction_decorator.rb
@@ -13,22 +13,22 @@ class TransferTransactionDecorator < ApplicationDecorator
     receiver_id.present? && receiver_id == context[:current_user]&.id
   end
 
-  def outcoming?
+  def outgoing?
     sender_id.present? && sender_id == context[:current_user]&.id
   end
 
-  def amount
-    format('%.2f', super.to_f)
+  def formated_amount
+    format('%.2f', amount)
   end
 
   def styled_amount
     if incoming?
-      ActionController::Base.helpers.content_tag('p', "+ #{amount} $", class: 'text-success')
-    elsif outcoming?
-      ActionController::Base.helpers.content_tag('p', "- #{amount} $", class: 'text-danger')
+      ActionController::Base.helpers.content_tag('p', "+ #{formated_amount} $", class: 'text-success')
+    elsif outgoing?
+      ActionController::Base.helpers.content_tag('p', "- #{formated_amount} $", class: 'text-danger')
     else
       # This case is not realistic on this stage, but it's for future use
-      ActionController::Base.helpers.content_tag('p', "#{amount} $", class: 'text-info')
+      ActionController::Base.helpers.content_tag('p', "#{formated_amount} $", class: 'text-info')
     end
   end
 end
--- spec/decorators/transfer_transaction_decorator_spec.rb
+++ spec/decorators/transfer_transaction_decorator_spec.rb
@@ -1,4 +1,104 @@
 require 'rails_helper'
 
 RSpec.describe TransferTransactionDecorator do
+  let(:sender) { create(:user) }
+  let(:receiver) { create(:user) }
+  let(:inner_transaction) { create(:transfer_transaction, sender:, receiver:, amount: 1917) }
+  let(:system_incoming_transaction) { create(:transfer_transaction, receiver:, amount: 1917) }
+  let(:system_outgoing_transaction) { create(:transfer_transaction, sender:, amount: 1917) }
+
+  before do
+    # add initial 100$ to subjects
+    create(:transfer_transaction, receiver:, amount: 2000)
+    create(:transfer_transaction, receiver: sender, amount: 2000)
+  end
+
+  describe '#sender_signature' do
+    it 'should return sender email if sender is not nil' do
+      expect(inner_transaction.decorate.sender_signature).to eq(sender.email)
+    end
+
+    it 'should return "DebitCredit (incoming transaction)" if sender is nil' do
+      expect(system_incoming_transaction.decorate.sender_signature).to eq('DebitCredit (incoming transaction)')
+    end
+  end
+
+  describe '#receiver_signature' do
+    it 'should return receiver email if receiver is not nil' do
+      expect(inner_transaction.decorate.receiver_signature).to eq(receiver.email)
+    end
+
+    it 'should return "DebitCredit (outgoing transaction)" if receiver is nil' do
+      expect(system_outgoing_transaction.decorate.receiver_signature).to eq('DebitCredit (outgoing transaction)')
+    end
+  end
+
+  describe '#formatted_amount' do
+    it 'should return formated amount' do
+      expect(inner_transaction.decorate.formated_amount).to eq('1917.00')
+    end
+  end
+
+  context 'context is not present in decorator' do
+    let(:decorated_transaction) { inner_transaction.decorate }
+
+    describe('#incoming?') do
+      it 'should return false' do
+        expect(decorated_transaction.incoming?).to eq(false)
+      end
+    end
+
+    describe('#outgoing?') do
+      it 'should return false' do
+        expect(decorated_transaction.outgoing?).to eq(false)
+      end
+    end
+    describe('#styled_amoount') do
+      it 'should return formatted amount' do
+        expect(decorated_transaction.styled_amount).to eq('<p class="text-info">1917.00 $</p>')
+      end
+    end
+  end
+
+  context 'sender is present in decorator context as current_user' do
+    let(:decorated_transaction) { inner_transaction.decorate(context: { current_user: sender }) }
+
+    describe('#incoming?') do
+      it 'should return true' do
+        expect(decorated_transaction.incoming?).to eq(false)
+      end
+    end
+
+    describe('#outgoing?') do
+      it 'should return false' do
+        expect(decorated_transaction.outgoing?).to eq(true)
+      end
+    end
+    describe('#styled_amoount') do
+      it 'should return formatted amount' do
+        expect(decorated_transaction.styled_amount).to eq('<p class="text-danger">- 1917.00 $</p>')
+      end
+    end
+
+    context 'receiver is present in decorator context as current_user' do
+      let(:decorated_transaction) { inner_transaction.decorate(context: { current_user: receiver }) }
+
+      describe('#incoming?') do
+        it 'should return false' do
+          expect(decorated_transaction.incoming?).to eq(true)
+        end
+      end
+
+      describe('#outgoing?') do
+        it 'should return true' do
+          expect(decorated_transaction.outgoing?).to eq(false)
+        end
+      end
+      describe('#styled_amoount') do
+        it 'should return formatted amount' do
+          expect(decorated_transaction.styled_amount).to eq('<p class="text-success">+ 1917.00 $</p>')
+        end
+      end
+    end
+  end
 end
